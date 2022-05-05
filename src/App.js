b4acf3af329d4429603198f70745d4e1df9a6207
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-05 22:06:30 +0200

    add context provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import FindRoute from './components/findRoute/FindRoute';
+import AddressForm from './components/addressForm/AddressForm';
 import LinksBanner from './components/linksBanner/LinksBanner';
 import Map from './components/map/Map';
+import CurrentRouteContextProvider from './contexts/CurrentRouteContext';
 
 function App() {
   return (
     <div className='App'>
       <header>JOURNEY COST CALCULATOR</header>
-      <Routes>
-        <Route path='/' element={<LinksBanner />} />
-        <Route path='find-addresses' element={<FindRoute />} />
-        <Route path='my-route' element={<Map />} />
-      </Routes>
+      <CurrentRouteContextProvider>
+        <Routes>
+          <Route path='/' element={<LinksBanner />} />
+          <Route path='find-addresses' element={<AddressForm />} />
+          <Route path='my-route' element={<Map />} />
+        </Routes>
+      </CurrentRouteContextProvider>
     </div>
   );
 }
