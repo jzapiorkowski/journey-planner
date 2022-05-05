21ad853c42461ea795e03353a119d7944671fa6c
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-05 18:17:10 +0200

    change form to handle both addresses

--- src/components/addressForm/AddressForm.js
+++ src/components/addressForm/AddressForm.js
@@ -5,27 +5,49 @@ function AddressForm() {
   return (
     <Formik
       initialValues={{
-        country: '',
-        city: '',
-        street: '',
-        streetNumber: null || '',
+        origin: {
+          country: '',
+          city: '',
+          street: '',
+          streetNumber: '',
+        },
+        destination: {
+          country: '',
+          city: '',
+          street: '',
+          streetNumber: '',
+        },
       }}
       onSubmit={(values) => {
         alert(JSON.stringify(values, null, 2));
       }}
     >
       <Form>
+        <h5>Origin Address</h5>
         <label htmlFor='country'>Country</label>
-        <Field name='country' type='text' />
+        <Field name='origin.country' type='text' />
 
         <label htmlFor='city'>City</label>
-        <Field name='city' type='text' />
+        <Field name='origin.city' type='text' />
 
         <label htmlFor='street'>Street</label>
-        <Field name='street' type='text' />
+        <Field name='origin.street' type='text' />
 
         <label htmlFor='streetNumber'>Street Number</label>
-        <Field name='streetNumber' type='text' />
+        <Field name='origin.streetNumber' type='text' />
+
+        <h5>Destination Address</h5>
+        <label htmlFor='country'>Country</label>
+        <Field name='destination.country' type='text' />
+
+        <label htmlFor='city'>City</label>
+        <Field name='destination.city' type='text' />
+
+        <label htmlFor='street'>Street</label>
+        <Field name='destination.street' type='text' />
+
+        <label htmlFor='streetNumber'>Street Number</label>
+        <Field name='destination.streetNumber' type='text' />
 
         <button type='submit'>Submit</button>
       </Form>
