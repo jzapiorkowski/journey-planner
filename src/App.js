277483250dd87b95c6b040f5ebb1cb1458bb3d66
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-05 23:30:48 +0200

    create route for all routes overview

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import AddressForm from './components/addressForm/AddressForm';
+import AllRoutesList from './components/allRoutesList/AllRoutesList';
 import LinksBanner from './components/linksBanner/LinksBanner';
 import Map from './components/map/Map';
 import AllRoutesContextProvider from './contexts/AllRoutesContext';
@@ -18,6 +19,7 @@ function App() {
             <Route path='/' element={<LinksBanner />} />
             <Route path='find-addresses' element={<AddressForm />} />
             <Route path='my-route' element={<Map />} />
+            <Route path='my-routes' element={<AllRoutesList />} />
           </Routes>
         </AllRoutesContextProvider>
       </CurrentRouteContextProvider>
