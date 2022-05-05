bdd1099345287e6aa1e76f025ae0d3c3e820e81a
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-05 22:02:41 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import FindRoute from './components/findRoute/FindRoute';
+import LinksBanner from './components/linksBanner/LinksBanner';
+import Map from './components/map/Map';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <header>JOURNEY COST CALCULATOR</header>
+      <Routes>
+        <Route path='/' element={<LinksBanner />} />
+        <Route path='find-addresses' element={<FindRoute />} />
+        <Route path='my-route' element={<Map />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
