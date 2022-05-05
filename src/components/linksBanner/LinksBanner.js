ab53213922cb5e68188daf30c7f04972534de210
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-05 22:06:42 +0200

    create component

---
+++ src/components/linksBanner/LinksBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function LinksBanner() {
+  return (
+    <div>
+      <Link to='/find-addresses'>Find new route</Link>
+    </div>
+  );
+}
+
+export default LinksBanner;
