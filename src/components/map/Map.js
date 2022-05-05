f721776740a2eeea964b1664a947d8530e5c1581
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-05 22:27:28 +0200

    add route to map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import RoutingMachine from '../routingMachine/RoutingMachine';
 import 'leaflet/dist/images/marker-shadow.png';
+import { currentRouteContext } from './../../contexts/CurrentRouteContext';
 
 function Map() {
+  const routeCoordinates = useContext(currentRouteContext);
+
   return (
     <div>
       <MapContainer
@@ -15,6 +19,10 @@ function Map() {
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <RoutingMachine
+          originAddressCoordinates={routeCoordinates[0]}
+          destinationAddressCoordinates={routeCoordinates[1]}
         />
       </MapContainer>
     </div>
