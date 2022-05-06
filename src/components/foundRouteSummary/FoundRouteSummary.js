1729e6cc460ce0b0f565abab7eef62c50ee59270
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-06 22:21:44 +0200

    move state to parent component

--- src/components/foundRouteSummary/FoundRouteSummary.js
+++ src/components/foundRouteSummary/FoundRouteSummary.js
@@ -1,11 +1,15 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Map from '../map/Map';
 import { currentRouteContext } from './../../contexts/CurrentRouteContext';
-import AddressNames from '../viewRoute/ViewRoute';
+import AddressNames from '../addressNames/AddressNames';
 import { useLocation } from 'react-router-dom';
+import ReverseGeocode from '../../utils/ReverseGeocode';
 
 function FoundRouteSummary() {
   const routeCoordinates = useContext(currentRouteContext);
+
+  const [originPlaceName, setOriginPlaceName] = useState([]);
+  const [destinationPlaceName, setDestinationPlaceName] = useState([]);
 
   const [totalDistance, setTotalDistance] = useState(0);
   const [routeInstructions, setRouteInstructions] = useState([]);
@@ -13,14 +17,28 @@ function FoundRouteSummary() {
   const location = useLocation();
   const { originGeolocation, destinationGeolocation } = location.state || {};
 
+  useEffect(() => {
+    ReverseGeocode(originGeolocation || routeCoordinates[0]).then(
+      (response) => {
+        const tmp = response.split(', ');
+        setOriginPlaceName(tmp);
+      }
+    );
+
+    ReverseGeocode(destinationGeolocation || routeCoordinates[1]).then(
+      (response) => {
+        const tmp = response.split(', ');
+        setDestinationPlaceName(tmp);
+      }
+    );
+  }, []);
+
   return (
     <div>
       <h1>Route Summary</h1>
       <AddressNames
-        originAddressCoordinates={originGeolocation || routeCoordinates[0]}
-        destinationAddressCoordinates={
-          destinationGeolocation || routeCoordinates[1]
-        }
+        originPlaceName={originPlaceName}
+        destinationPlaceName={destinationPlaceName}
       />
       <p>Your route is {totalDistance} meters long</p>
       <ul>
