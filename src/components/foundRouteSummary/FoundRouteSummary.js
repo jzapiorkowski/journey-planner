1325cc5f24980bbc45273f0eb8d15e7b94c86117
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-06 13:00:18 +0200

    add addresses info

--- src/components/foundRouteSummary/FoundRouteSummary.js
+++ src/components/foundRouteSummary/FoundRouteSummary.js
@@ -1,15 +1,18 @@
 import React, { useContext, useState } from 'react';
 import Map from '../map/Map';
 import { currentRouteContext } from './../../contexts/CurrentRouteContext';
+import ViewRoute from '../viewRoute/ViewRoute';
 
 function FoundRouteSummary() {
   const routeCoordinates = useContext(currentRouteContext);
+
   const [totalDistance, setTotalDistance] = useState(0);
   const [routeInstructions, setRouteInstructions] = useState([]);
 
   return (
     <div>
       <h1>Route Summary</h1>
+      <ViewRoute></ViewRoute>
       <p>Your route is {totalDistance} meters long</p>
       <ul>
         {routeInstructions.map((instruction, index) => {
