23e18cce86a89fa06df70917ef5e45e3f82a49ed
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-06 12:14:52 +0200

    create function

---
+++ src/utils/ReverseGeocode.js
@@ -0,0 +1,18 @@
+import geolocationClient from '../api/GeolocationClient';
+
+function ReverseGeocode(address) {
+  const personalToken = ''; // put your mapbox personal access token here
+
+  const formattedAddress = address.reverse().join(',');
+
+  return geolocationClient
+    .get(`/${formattedAddress}.json?access_token=${personalToken}`)
+    .then((response) => {
+      return response.data.features[0].place_name;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export default ReverseGeocode;
