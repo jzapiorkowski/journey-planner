4d5cd8a75a46884d2fbc3c89ca6c5d5bab1dee96
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-06 13:00:39 +0200

    make component reusable

--- src/components/viewRoute/ViewRoute.js
+++ src/components/viewRoute/ViewRoute.js
@@ -1,29 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReverseGeocode from '../../utils/ReverseGeocode';
+import { currentRouteContext } from './../../contexts/CurrentRouteContext';
 
 function ViewRoute() {
+  const routeCoordinates = useContext(currentRouteContext);
+
   const location = useLocation();
-  const { originGeolocation, destinationGeolocation } = location.state;
+  const { originGeolocation, destinationGeolocation } = location.state || {};
 
   const [originPlaceName, setOriginPlaceName] = useState([]);
   const [destinationPlaceName, setDestinationPlaceName] = useState([]);
 
   useEffect(() => {
-    ReverseGeocode(originGeolocation).then((response) => {
-      const tmp = response.split(', ');
-      setOriginPlaceName(tmp);
-    });
+    console.log(routeCoordinates, 'origin');
+    ReverseGeocode(originGeolocation || routeCoordinates[0]).then(
+      (response) => {
+        const tmp = response.split(', ');
+        setOriginPlaceName(tmp);
+      }
+    );
 
-    ReverseGeocode(destinationGeolocation).then((response) => {
-      const tmp = response.split(', ');
-      setDestinationPlaceName(tmp);
-    });
+    ReverseGeocode(destinationGeolocation || routeCoordinates[1]).then(
+      (response) => {
+        const tmp = response.split(', ');
+        setDestinationPlaceName(tmp);
+      }
+    );
   }, []);
 
   return (
     <div>
-      <h1>Route summary</h1>
       <div className='address' id='origin-address'>
         <h3>Origin Address</h3>
         {originPlaceName.map((element, index) => {
