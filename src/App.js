266f186c70c3f619c3f153b6a123dfc48fbe9cdc
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-07 18:26:27 +0200

    delete unused context provider

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import FoundRouteSummary from './components/foundRouteSummary/FoundRouteSummary'
 import LinksBanner from './components/linksBanner/LinksBanner';
 import RouteNotFound from './components/routeNotFound/RouteNotFound';
 import AllRoutesContextProvider from './contexts/AllRoutesContext';
-import CurrentRouteContextProvider from './contexts/CurrentRouteContext';
 
 function App() {
   return (
@@ -14,32 +13,30 @@ function App() {
       <header>
         <Link to='/'>JOURNEY COST CALCULATOR</Link>
       </header>
-      <CurrentRouteContextProvider>
-        <AllRoutesContextProvider>
-          <Routes>
-            <Route path='/' element={<LinksBanner />} />
-            <Route path='find-addresses' element={<AddressForm />} />
-            <Route path='my-route' element={<FoundRouteSummary />} />
-            <Route path='my-routes' element={<AllRoutesList />} />
-            <Route path='/route/:routeId' element={<FoundRouteSummary />} />
-            <Route path='route-not-found' element={<RouteNotFound />} />
-            <Route
-              path='*'
-              element={
-                <p
-                  style={{
-                    textAlign: 'center',
-                    marginTop: '50px',
-                    fontSize: '30px',
-                  }}
-                >
-                  Sorry, but we couldn't find the thing you were looking for
-                </p>
-              }
-            />
-          </Routes>
-        </AllRoutesContextProvider>
-      </CurrentRouteContextProvider>
+      <AllRoutesContextProvider>
+        <Routes>
+          <Route path='/' element={<LinksBanner />} />
+          <Route path='find-addresses' element={<AddressForm />} />
+          <Route path='my-route' element={<FoundRouteSummary />} />
+          <Route path='my-routes' element={<AllRoutesList />} />
+          <Route path='/route/:routeId' element={<FoundRouteSummary />} />
+          <Route path='route-not-found' element={<RouteNotFound />} />
+          <Route
+            path='*'
+            element={
+              <p
+                style={{
+                  textAlign: 'center',
+                  marginTop: '50px',
+                  fontSize: '30px',
+                }}
+              >
+                Sorry, but we couldn't find the thing you were looking for
+              </p>
+            }
+          />
+        </Routes>
+      </AllRoutesContextProvider>
     </div>
   );
 }
