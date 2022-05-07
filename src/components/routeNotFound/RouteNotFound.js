fcced33d9bd57f3a7271f167116ccb2d6304b523
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-05-07 17:27:12 +0200

    create component

---
+++ src/components/routeNotFound/RouteNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RouteNotFound() {
+  return (
+    <div className='route-not-found'>
+      <p>Sorry, but we could't find the route you were looking for</p>
+    </div>
+  );
+}
+
+export default RouteNotFound;
