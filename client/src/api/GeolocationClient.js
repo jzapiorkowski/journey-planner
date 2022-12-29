2bfc35f849841775c39b6725aa9475c755d6026c
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-12-29 17:43:05 +0100

    remove unused file

--- client/src/api/GeolocationClient.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios').default;
-
-const geolocationClient = axios.create({
-  baseURL: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
-});
-
-export default geolocationClient;
